070af3b6b2db68b2170ea1b0a12650a0f7504f5c
FadimeZgj <fadime.ilhan5703@gmail.com>
2024-05-01 00:56:55 +0200

    ✨Authentication JWT

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api')]
 class QuizController extends AbstractController
 {
     #[Route('/quizzes', name: 'app_quizzes')]
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,14 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $faker = Factory::create();
 
         $manager->flush();
     }
